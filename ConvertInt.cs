1efe07fdfa9def718cb3662f791a5bc672041319
Мусин Михаил <musin.mihail@gmail.com>
2022-07-24 21:30:40 +0300

    Копейки

--- ConvertInt.cs
+++ ConvertInt.cs
@@ -4,81 +4,173 @@ namespace Course
     public class ConvertInt
     {
         List<int> listInt = new List<int>();
-
-        public long number = 908070605040123;
+        List<int> listIntKopeck = new List<int>();
+        public string number = "";
+        bool error = false;
+        string result = "";
         public void StartGame()
         {
-            string result = Convert();
+            Console.Clear();
+            if (result.Length > 0)
+            {
+                Console.WriteLine(result);
+            }
+            Console.WriteLine("Введите сумму. Пример: \"34\", \"22,87\", \"1567.45\"");
+            number = Console.ReadLine();
+            Console.Clear();
+            result = Convert();
             Console.WriteLine(result);
-            Console.ReadLine();
+            if (error == true)
+            {
+                error = false;
+                StartGame();
+            }
+            else
+            {
+                Console.ReadLine();
+            }
         }
         public string Convert()
         {
+            foreach (char char2 in number)
+            {
+                if (Char.IsNumber(char2) == false)
+                {
+                    if (char2 != ',' && char2 != '.')
+                    {
+                        Console.Clear();
+                        error = true;
+                        return "Неверный формат ввода";
+                    }
+                }
+            }
             IntToList(number);
-            string newString = "";
-            newString = numberTwo(1, "") + newString;
-            newString = numberTwo(4, " тысяч ") + newString;
-            newString = numberTwo(7, " миллионов ") + newString;
-            newString = numberTwo(10, " миллиардов ") + newString;
-            newString = numberTwo(13, " триллионов ") + newString;
-            return newString;
-        }
-        void IntToList(long number)
-        {
-            string numberString = number.ToString();
+            string kopecks = "";
+            kopecks = IntToString(1, " копеек") + kopecks;
+            string rub = "";
+            rub = IntToString(1, " рублей ") + rub;
+            rub = IntToString(4, " тысяч ") + rub;
+            rub = IntToString(7, " миллионов ") + rub;
+            rub = IntToString(10, " миллиардов ") + rub;
+            rub = IntToString(13, " триллионов ") + rub;
+            if (rub.Length == 0)
+            {
+                rub = "ноль рублей ";
+            }
 
+            return rub + kopecks;
+        }
+        void IntToList(string number)
+        {
+            var number2 = number.Split('.', ',');
             for (int i = 0; i < 20; i++)
             {
-                if (i < numberString.Length)
+                if (i < number2[0].Length)
                 {
-                    listInt.Insert(0, numberString[i] - '0');
+                    listInt.Insert(0, number2[0][i] - '0');
                 }
                 else
                 {
                     listInt.Add(0);
                 }
             }
-        }
-        string numberTwo(int index, string name)
-        {
-            string newString = "";
-            if (listInt[index] == 1)
+            if (number2.Length > 1)
             {
-                newString = div12(listInt[index - 1]) + name + newString;
+                for (int i = 0; i < 2; i++)
+                {
+                    if (i < number2[1].Length)
+                    {
+                        listIntKopeck.Insert(0, number2[1][i] - '0');
+                    }
+                    else
+                    {
+                        listIntKopeck.Add(0);
+                    }
+                }
             }
             else
             {
-                if (name == "")
-                {
-                    newString = div1(listInt[index - 1]) + newString;
-                }
-                else if (name == " тысяч ")
-                {
-                    newString = div1Thousand(listInt[index - 1]) + newString;
-                }
-                else if (name == " миллионов ")
-                {
-                    newString = div1Million(listInt[index - 1]) + newString;
-                }
-                else if (name == " миллиардов ")
-                {
-                    newString = div1Billion(listInt[index - 1]) + newString;
-                }
-                else if (name == " триллионов ")
-                {
-                    newString = div1Trillion(listInt[index - 1]) + newString;
-                }
-                if (listInt[index] != 0 && listInt[index - 1] == 0)
-                {
-                    newString = name + newString;
-                }
-                if (listInt[index] != 0 && listInt[index - 1] != 0)
-                {
-                    newString = " " + newString;
-                }
-                newString = div10(listInt[index]) + newString;
+                listIntKopeck.Add(0);
+                listIntKopeck.Add(0);
             }
-            newString = numberOne(index + 2, name) + newString;
+        }
+        string IntToString(int index, string name)
+        {
+            string newString = "";
+            if (name == " копеек")
+            {
+                if (listIntKopeck[index] == 1)
+                {
+                    newString = div12(listIntKopeck[index - 1]) + name + newString;
+                }
+                else
+                {
+                    if (listIntKopeck.Count > 0)
+                    {
+                        if (listIntKopeck[0] == 0 && listIntKopeck[1] == 0)
+                        {
+                            newString = "ноль копеек" + newString;
+                        }
+                        else if (listIntKopeck[1] == 0)
+                        {
+                            newString = div1Kopeck(listIntKopeck[index - 1]) + newString;
+                        }
+                        else if (listIntKopeck[0] == 0)
+                        {
+                            newString = div10(listIntKopeck[index]) + name + newString;
+                        }
+                        else
+                        {
+                            newString = div1Kopeck(listIntKopeck[index - 1]) + newString;
+                            newString = div10(listIntKopeck[index]) + " " + newString;
+                        }
+                    }
+                    else
+                    {
+                        newString = "ноль копеек" + newString;
+                    }
+                }
+            }
+            else
+            {
+                if (listInt[index] == 1)
+                {
+                    newString = div12(listInt[index - 1]) + name + newString;
+                }
+                else
+                {
+                    if (name == " рублей ")
+                    {
+                        newString = div1(listInt[index - 1]) + newString;
+                    }
+                    else if (name == " тысяч ")
+                    {
+                        newString = div1Thousand(listInt[index - 1]) + newString;
+                    }
+                    else if (name == " миллионов ")
+                    {
+                        newString = div1Million(listInt[index - 1]) + newString;
+                    }
+                    else if (name == " миллиардов ")
+                    {
+                        newString = div1Billion(listInt[index - 1]) + newString;
+                    }
+                    else if (name == " триллионов ")
+                    {
+                        newString = div1Trillion(listInt[index - 1]) + newString;
+                    }
+                    if (listInt[index] != 0 && listInt[index - 1] == 0)
+                    {
+                        newString = name + newString;
+                    }
+                    if (listInt[index] != 0 && listInt[index - 1] != 0)
+                    {
+                        newString = " " + newString;
+                    }
+                    newString = div10(listInt[index]) + newString;
+                }
+                newString = numberOne(index + 2, name) + newString;
+            }
             return newString;
         }
         string numberOne(int index, string name)
@@ -98,52 +190,9 @@ namespace Course
             newString = div100(listInt[index - 1]) + newString;
             return newString;
         }
-        string div1(int number)
-        {
-            string newString = "";
-            if (number == 1)
-            {
-                newString = "один";
-            }
-            else if (number == 2)
-            {
-                newString = "два";
-            }
-            else if (number == 3)
-            {
-                newString = "три";
-            }
-            else if (number == 4)
-            {
-                newString = "четыре";
-            }
-            else if (number == 5)
-            {
-                newString = "пять";
-            }
-            else if (number == 6)
-            {
-                newString = "шесть";
-            }
-            else if (number == 7)
-            {
-                newString = "семь";
-            }
-            else if (number == 8)
-            {
-                newString = "восемь";
-            }
-            else if (number == 9)
-            {
-                newString = "девять";
-            }
-
-            return newString;
-        }
         string div10(int number)
         {
             string newString = "";
-
             if (number == 1)
             {
                 newString = "десять";
@@ -170,7 +219,7 @@ namespace Course
             }
             else if (number == 7)
             {
-                newString = "семдесять";
+                newString = "семдесят";
             }
             else if (number == 8)
             {
@@ -266,13 +315,93 @@ namespace Course
             {
                 newString = "девятсот";
             }
-
+            return newString;
+        }
+        string div1Kopeck(int number)
+        {
+            string newString = "";
+            if (number == 1)
+            {
+                newString = "одна копейка";
+            }
+            else if (number == 2)
+            {
+                newString = "две копейки";
+            }
+            else if (number == 3)
+            {
+                newString = "три копейки";
+            }
+            else if (number == 4)
+            {
+                newString = "четыре копейки";
+            }
+            else if (number == 5)
+            {
+                newString = "пять копеек";
+            }
+            else if (number == 6)
+            {
+                newString = "шесть копеек";
+            }
+            else if (number == 7)
+            {
+                newString = "семь копеек";
+            }
+            else if (number == 8)
+            {
+                newString = "восемь копеек";
+            }
+            else if (number == 9)
+            {
+                newString = "девять копеек";
+            }
+            return newString;
+        }
+        string div1(int number)
+        {
+            string newString = "";
+            if (number == 1)
+            {
+                newString = "один рубль ";
+            }
+            else if (number == 2)
+            {
+                newString = "два рубля ";
+            }
+            else if (number == 3)
+            {
+                newString = "три рубля ";
+            }
+            else if (number == 4)
+            {
+                newString = "четыре рубля ";
+            }
+            else if (number == 5)
+            {
+                newString = "пять рубля ";
+            }
+            else if (number == 6)
+            {
+                newString = "шесть рублей ";
+            }
+            else if (number == 7)
+            {
+                newString = "семь рублей ";
+            }
+            else if (number == 8)
+            {
+                newString = "восемь рублей ";
+            }
+            else if (number == 9)
+            {
+                newString = "девять рублей ";
+            }
             return newString;
         }
         string div1Thousand(int number)
         {
             string newString = "";
-
             if (number == 1)
             {
                 newString = "одна тысяча";
@@ -318,7 +447,6 @@ namespace Course
         string div1Million(int number)
         {
             string newString = "";
-
             if (number == 1)
             {
                 newString = "один миллион";
@@ -364,7 +492,6 @@ namespace Course
         string div1Billion(int number)
         {
             string newString = "";
-
             if (number == 1)
             {
                 newString = "один миллиард ";
@@ -410,7 +537,6 @@ namespace Course
         string div1Trillion(int number)
         {
             string newString = "";
-
             if (number == 1)
             {
                 newString = "один триллион ";
